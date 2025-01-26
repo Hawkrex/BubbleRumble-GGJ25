963f72d09ec2141e75fb9b9fa4883015fade7328
Hawkrex <Hawkrex50@gmail.com>
2025-01-26 11:57:03 +0100

    Doubled circle for CoreBubble to make it less crappy ingame / Added enemy AI that follows player when close enough

--- Scripts/Enemy.cs
+++ Scripts/Enemy.cs
@@ -1,9 +1,18 @@
 using Godot;
 
-public partial class Enemy : RigidBody2D
+public partial class Enemy : CharacterBody2D
 {
 	[Signal]
 	public delegate void EnemyDeadEventHandler(Enemy enemy, int score);
+
+	[Export]
+	public int Health { get; set; } = 1;
+
+	[Export]
+	public float MovementSpeed { get; set; } = 100f;
+
+	[Export]
+	public float FireRate { get; set; } = 1f;
 
 	public void DecreaseHealth()
 	{
---
+++ Scripts/EnemyAi.cs
@@ -0,0 +1,54 @@
+using Godot;
+using System;
+
+public partial class EnemyAi : Node2D
+{
+	[Export]
+	public Area2D PlayerDetectionZone { get; set; }
+
+	[Export]
+	public Enemy Enemy { get; set; }
+
+	private bool attackPlayer = false;
+	private Player player;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		PlayerDetectionZone.BodyEntered += PlayerDetectionZone_BodyEntered;
+		PlayerDetectionZone.BodyExited += PlayerDetectionZone_BodyExited;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (attackPlayer)
+		{
+			Enemy.Velocity = (player.Position - Enemy.Position).Normalized() * Enemy.MovementSpeed;
+			Enemy.LookAt(player.Position);
+			Enemy.MoveAndSlide();
+		}
+	}
+
+	private void PlayerDetectionZone_BodyEntered(Node2D body)
+	{
+		var detectedBody = body as Player;
+		if (detectedBody == null)
+		{
+			return;
+		}
+		player = detectedBody;
+		attackPlayer = true;
+	}
+
+	private void PlayerDetectionZone_BodyExited(Node2D body)
+	{
+		var detectedBody = body as Player;
+		if (detectedBody == null)
+		{
+			return;
+		}
+		player = detectedBody;
+		attackPlayer = false;
+	}
+}
